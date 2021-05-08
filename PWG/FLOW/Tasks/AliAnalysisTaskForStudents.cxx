626d4de1064c8a6d3f2f0e072fa24fbe4d6ad0e6
Anton Riedel <anton.riedel@tum.de>
2021-05-08 13:55:12 +0200

    Challenge 01

--- PWG/FLOW/Tasks/AliAnalysisTaskForStudents.cxx
+++ PWG/FLOW/Tasks/AliAnalysisTaskForStudents.cxx
@@ -1,27 +1,34 @@
+/**
+ * File              : AliAnalysisTaskForStudents.cxx
+ * Author            : Anton Riedel <anton.riedel@tum.de>
+ * Date              : 07.05.2021
+ * Last Modified Date: 08.05.2021
+ * Last Modified By  : Anton Riedel <anton.riedel@tum.de>
+ */
 /*************************************************************************
-* Copyright(c) 1998-2008, ALICE Experiment at CERN, All rights reserved. *
-*                                                                        *
-* Author: The ALICE Off-line Project.                                    *
-* Contributors are mentioned in the code where appropriate.              *
-*                                                                        *
-* Permission to use, copy, modify and distribute this software and its   *
-* documentation strictly for non-commercial purposes is hereby granted   *
-* without fee, provided that the above copyright notice appears in all   *
-* copies and that both the copyright notice and this permission notice   *
-* appear in the supporting documentation. The authors make no claims     *
-* about the suitability of this software for any purpose. It is          *
-* provided "as is" without express or implied warranty.                  * 
-**************************************************************************/
+ * Copyright(c) 1998-2008, ALICE Experiment at CERN, All rights reserved. *
+ *                                                                        *
+ * Author: The ALICE Off-line Project.                                    *
+ * Contributors are mentioned in the code where appropriate.              *
+ *                                                                        *
+ * Permission to use, copy, modify and distribute this software and its   *
+ * documentation strictly for non-commercial purposes is hereby granted   *
+ * without fee, provided that the above copyright notice appears in all   *
+ * copies and that both the copyright notice and this permission notice   *
+ * appear in the supporting documentation. The authors make no claims     *
+ * about the suitability of this software for any purpose. It is          *
+ * provided "as is" without express or implied warranty.                  *
+ **************************************************************************/
 
-/************************************** 
-* template class for student projects * 
-**************************************/ 
-  
-#include "Riostream.h"
-#include "AliAnalysisTaskForStudents.h"
-#include "AliLog.h"
+/**************************************
+ * template class for student projects *
+ **************************************/
+
 #include "AliAODEvent.h"
 #include "AliAODInputHandler.h"
+#include "AliAnalysisTaskForStudents.h"
+#include "AliLog.h"
+#include "Riostream.h"
 #include "TFile.h"
 
 using std::cout;
@@ -29,23 +36,25 @@ using std::endl;
 
 ClassImp(AliAnalysisTaskForStudents)
 
-//================================================================================================================
+    //================================================================================================================
 
-AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(const char *name, Bool_t useParticleWeights): 
- AliAnalysisTaskSE(name), 
- fHistList(NULL),
- // Control histograms:
- fControlHistogramsList(NULL),
- fPtHist(NULL),
- fNbinsPt(1000),
- fMinBinPt(0.),
- fMaxBinPt(10.),
- // Final results:
- fFinalResultsList(NULL)
- {
+    AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(
+        const char *name, Bool_t useParticleWeights)
+    : AliAnalysisTaskSE(name), fHistList(NULL),
+      // Control histograms:
+      fControlHistogramsList(NULL), fPtHist(NULL), fNbinsPt(1000),
+      fMinBinPt(0.), fMaxBinPt(10.), fPhiHist(NULL), fNbinsPhi(1000),
+      fMinBinPhi(0.), fMaxBinPhi(10.), fEtaHist(NULL), fNbinsEta(1000),
+      fMinBinEta(0.), fMaxBinEta(10.), fMulHist(NULL), fNbinsMul(1000),
+      fMinBinMul(0.), fMaxBinMul(10.),
+
+      // Final results:
+      fFinalResultsList(NULL), fAveragePhiHist(NULL), fNbinsAveragePhi(1000),
+      fMinBinAveragePhi(0.), fMaxBinAveragePhi(10.) {
   // Constructor.
- 
-  AliDebug(2,"AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(const char *name, Bool_t useParticleWeights)");
+
+  AliDebug(2, "AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(const "
+              "char *name, Bool_t useParticleWeights)");
 
   // Base list:
   fHistList = new TList();
@@ -57,157 +66,171 @@ AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(const char *name, Bool_t 
 
   // Define input and output slots here
   // Input slot #0 works with an AliFlowEventSimple
-  //DefineInput(0, AliFlowEventSimple::Class());  
+  // DefineInput(0, AliFlowEventSimple::Class());
   // Input slot #1 is needed for the weights input file:
-  //if(useParticleWeights)
+  // if(useParticleWeights)
   //{
-  // DefineInput(1, TList::Class());   
-  //}  
-  // Output slot #0 is reserved              
+  // DefineInput(1, TList::Class());
+  //}
+  // Output slot #0 is reserved
   // Output slot #1 writes into a TList container
 
-  DefineOutput(1, TList::Class());  
+  DefineOutput(1, TList::Class());
 
-  if(useParticleWeights)
-  {
-   // not needed for the time being
+  if (useParticleWeights) {
+    // not needed for the time being
   }
 
-} // AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(const char *name, Bool_t useParticleWeights): 
+} // AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(const char *name,
+  // Bool_t useParticleWeights):
 
 //================================================================================================================
 
-AliAnalysisTaskForStudents::AliAnalysisTaskForStudents(): 
- AliAnalysisTaskSE(),
- fHistList(NULL),
- // Control histograms:
- fControlHistogramsList(NULL),
- fPtHist(NULL),
- fNbinsPt(1000),
- fMinBinPt(0.),
- fMaxBinPt(10.),
- // Final results:
- fFinalResultsList(NULL)
-{
+AliAnalysisTaskForStudents::AliAnalysisTaskForStudents()
+    : AliAnalysisTaskSE(), fHistList(NULL),
+      // Control histograms:
+      fControlHistogramsList(NULL), fPtHist(NULL), fNbinsPt(1000),
+      fMinBinPt(0.), fMaxBinPt(10.), fPhiHist(NULL), fNbinsPhi(1000),
+      fMinBinPhi(0.), fMaxBinPhi(10.), fEtaHist(NULL), fNbinsEta(1000),
+      fMinBinEta(0.), fMaxBinEta(10.), fMulHist(NULL), fNbinsMul(1000),
+      fMinBinMul(0.), fMaxBinMul(10.),
+      // Final results:
+      fFinalResultsList(NULL) {
   // Dummy constructor.
- 
-  AliDebug(2,"AliAnalysisTaskForStudents::AliAnalysisTaskForStudents()");
+
+  AliDebug(2, "AliAnalysisTaskForStudents::AliAnalysisTaskForStudents()");
 
 } // AliAnalysisTaskForStudents::AliAnalysisTaskForStudents():
 
 //================================================================================================================
 
-AliAnalysisTaskForStudents::~AliAnalysisTaskForStudents()
-{
- // Destructor.
+AliAnalysisTaskForStudents::~AliAnalysisTaskForStudents() {
+  // Destructor.
 
- if(fHistList) delete fHistList;
-  
+  if (fHistList)
+    delete fHistList;
+
 } // AliAnalysisTaskForStudents::~AliAnalysisTaskForStudents()
 
 //================================================================================================================
 
-void AliAnalysisTaskForStudents::UserCreateOutputObjects() 
-{
- // Called at every worker node to initialize.
+void AliAnalysisTaskForStudents::UserCreateOutputObjects() {
+  // Called at every worker node to initialize.
 
- // a) Trick to avoid name clashes, part 1;
- // b) Book and nest all lists;
- // c) Book all objects;
- // *) Trick to avoid name clashes, part 2.
-  
- // a) Trick to avoid name clashes, part 1:
- Bool_t oldHistAddStatus = TH1::AddDirectoryStatus(); 
- TH1::AddDirectory(kFALSE);
+  // a) Trick to avoid name clashes, part 1;
+  // b) Book and nest all lists;
+  // c) Book all objects;
+  // *) Trick to avoid name clashes, part 2.
 
- // b) Book and nest all lists:
- this->BookAndNestAllLists();
+  // a) Trick to avoid name clashes, part 1:
+  Bool_t oldHistAddStatus = TH1::AddDirectoryStatus();
+  TH1::AddDirectory(kFALSE);
 
- // c) Book all objects:
- this->BookControlHistograms();
- this->BookFinalResultsHistograms();
+  // b) Book and nest all lists:
+  this->BookAndNestAllLists();
 
- // *) Trick to avoid name clashes, part 2:
- TH1::AddDirectory(oldHistAddStatus);
+  // c) Book all objects:
+  this->BookControlHistograms();
+  this->BookFinalResultsHistograms();
 
- PostData(1,fHistList);
+  // *) Trick to avoid name clashes, part 2:
+  TH1::AddDirectory(oldHistAddStatus);
 
-} // void AliAnalysisTaskForStudents::UserCreateOutputObjects() 
+  PostData(1, fHistList);
+
+} // void AliAnalysisTaskForStudents::UserCreateOutputObjects()
 
 //================================================================================================================
 
-void AliAnalysisTaskForStudents::UserExec(Option_t *) 
-{
- // Main loop (called for each event).
- // a) Get pointer to AOD event:
- // b) Start analysis over AODs;
- // c) Reset event-by-event objects;
- // d) PostData.
+void AliAnalysisTaskForStudents::UserExec(Option_t *) {
+  // Main loop (called for each event).
+  // a) Get pointer to AOD event:
+  // b) Start analysis over AODs;
+  // c) Reset event-by-event objects;
+  // d) PostData.
 
- // a) Get pointer to AOD event:
- AliAODEvent *aAOD = dynamic_cast<AliAODEvent*>(InputEvent()); // from TaskSE
- if(!aAOD){return;}
+  // a) Get pointer to AOD event:
+  AliAODEvent *aAOD = dynamic_cast<AliAODEvent *>(InputEvent()); // from TaskSE
+  if (!aAOD) {
+    return;
+  }
 
- // b) Start analysis over AODs:
- Int_t nTracks = aAOD->GetNumberOfTracks(); // number of all tracks in current event 
- for(Int_t iTrack=0;iTrack<nTracks;iTrack++) // starting a loop over all tracks
- {
-  AliAODTrack *aTrack = dynamic_cast<AliAODTrack*>(aAOD->GetTrack(iTrack)); // getting a pointer to a track
-  if(!aTrack){continue;} // protection against NULL pointers
-  if(!aTrack->TestFilterBit(128)){continue;} // filter bit 128 denotes TPC-only tracks, use only them for the analysis
+  // b) Start analysis over AODs:
+  Int_t nTracks =
+      aAOD->GetNumberOfTracks(); // number of all tracks in current event
 
-  // example variables for each track: (for more options, please see class STEER/AOD/AliAODTrack.h)  
-  Double_t pt = aTrack->Pt(); // Pt
-  //Double_t px = aTrack->Px(); // x-component of momenta
-  //Double_t py = aTrack->Py(); // y-component of momenta
-  //Double_t pz = aTrack->Pz(); // z-component of momenta
-  //Double_t e = aTrack->E();  // energy
-  //Double_t phi = aTrack->Phi(); // azimuthal angle
-  //Double_t eta = aTrack->Eta(); // pseudorapidity
-  //Double_t charge = aTrack->Charge(); // charge
- 
-  // apply some cuts: e.g. take for the analysis only particles in 0.2 < pT < 5.0 GeV
-  if( (0.2 < pt) && (pt < 5.0) ) // example cuts
+  fMulHist->Fill(nTracks);
+
+  for (Int_t iTrack = 0; iTrack < nTracks;
+       iTrack++) // starting a loop over all tracks
   {
-   // fill some control histograms:
-   fPtHist->Fill(pt); // filling pt distribution
- 
-   // do some analysis only with the particles which passed the cuts
-   // ... your analysis code ... 
+    AliAODTrack *aTrack = dynamic_cast<AliAODTrack *>(
+        aAOD->GetTrack(iTrack)); // getting a pointer to a track
+    if (!aTrack) {
+      continue;
+    } // protection against NULL pointers
+    if (!aTrack->TestFilterBit(128)) {
+      continue;
+    } // filter bit 128 denotes TPC-only tracks, use only them for the analysis
 
-  } // if( (0.2 < pT) && (pT < 5.0) )
+    // example variables for each track: (for more options, please see class
+    // STEER/AOD/AliAODTrack.h)
+    Double_t pt = aTrack->Pt();   // Pt
+    Double_t phi = aTrack->Phi(); // azimuthal angle
+    Double_t eta = aTrack->Eta(); // pseudorapidity
+    // Double_t px = aTrack->Px(); // x-component of momenta
+    // Double_t py = aTrack->Py(); // y-component of momenta
+    // Double_t pz = aTrack->Pz(); // z-component of momenta
+    // Double_t e = aTrack->E();  // energy
+    // Double_t charge = aTrack->Charge(); // charge
 
- } // for(Int_t iTrack=0;iTrack<nTracks;iTrack++) // starting a loop over all tracks
-  
- // c) Reset event-by-event objects:
- // ...
+    // apply some cuts: e.g. take for the analysis only particles in 0.2 < pT
+    // < 5.0 GeV
+    if ((0.2 < pt) && (pt < 5.0)) // example cuts
+    {
+      // fill some control histograms:
+      fPtHist->Fill(pt); // filling pt distribution
+      fPhiHist->Fill(phi);
+      fEtaHist->Fill(eta);
 
- // d) PostData:
- PostData(1,fHistList);
+      // do some analysis only with the particles which passed the cuts
+      // ... your analysis code ...
+
+    } // if( (0.2 < pT) && (pT < 5.0) )
+
+  } // for(Int_t iTrack=0;iTrack<nTracks;iTrack++) // starting a loop over all
+    // tracks
+
+  // c) Reset event-by-event objects:
+  // ...
+
+  // d) PostData:
+  PostData(1, fHistList);
 
 } // void AliAnalysisTaskForStudents::UserExec(Option_t *)
 
 //================================================================================================================
 
-void AliAnalysisTaskForStudents::Terminate(Option_t *)
-{
- // Accessing the merged output list.
+void AliAnalysisTaskForStudents::Terminate(Option_t *) {
+  // Accessing the merged output list.
 
- fHistList = (TList*)GetOutputData(1);
- if(!fHistList){exit(1);}
+  fHistList = (TList *)GetOutputData(1);
+  if (!fHistList) {
+    exit(1);
+  }
 
- // Do some calculation in offline mode here:
+  // Do some calculation in offline mode here:
 
- // ... your code for offline calculations ...
+  // ... your code for offline calculations ...
 
+  fAveragePhiHist->SetBinContent(1, fPhiHist->GetMean());
 
 } // end of void AliAnalysisTaskForStudents::Terminate(Option_t *)
 
 //================================================================================================================
 
-void AliAnalysisTaskForStudents::InitializeArrays()
-{
- // Initialize all data members which are arrays in this method.
+void AliAnalysisTaskForStudents::InitializeArrays() {
+  // Initialize all data members which are arrays in this method.
 
  // This is important, since these objects cannot be initialized directly in the constructor list. 
 
@@ -215,57 +238,80 @@ void AliAnalysisTaskForStudents::InitializeArrays()
 
 //=======================================================================================================================
 
-void AliAnalysisTaskForStudents::BookAndNestAllLists()
-{
- // Book and nest all lists nested in the base list fHistList.
+void AliAnalysisTaskForStudents::BookAndNestAllLists() {
+  // Book and nest all lists nested in the base list fHistList.
 
- // a) Book and nest lists for control histograms;
- // b) Book and nest lists for final results.
+  // a) Book and nest lists for control histograms;
+  // b) Book and nest lists for final results.
 
- TString sMethodName = "void AliAnalysisTaskForStudents::BookAndNestAllLists()";
- if(!fHistList){Fatal(sMethodName.Data(),"fHistList is NULL");}
+  TString sMethodName =
+      "void AliAnalysisTaskForStudents::BookAndNestAllLists()";
+  if (!fHistList) {
+    Fatal(sMethodName.Data(), "fHistList is NULL");
+  }
 
- // a) Book and nest lists for control histograms:
- fControlHistogramsList = new TList();
- fControlHistogramsList->SetName("ControlHistograms");
- fControlHistogramsList->SetOwner(kTRUE);
- fHistList->Add(fControlHistogramsList);
+  // a) Book and nest lists for control histograms:
+  fControlHistogramsList = new TList();
+  fControlHistogramsList->SetName("ControlHistograms");
+  fControlHistogramsList->SetOwner(kTRUE);
+  fHistList->Add(fControlHistogramsList);
 
- // b) Book and nest lists for final results:
- fFinalResultsList = new TList();
- fFinalResultsList->SetName("FinalResults");
- fFinalResultsList->SetOwner(kTRUE);
- fHistList->Add(fFinalResultsList);
+  // b) Book and nest lists for final results:
+  fFinalResultsList = new TList();
+  fFinalResultsList->SetName("FinalResults");
+  fFinalResultsList->SetOwner(kTRUE);
+  fHistList->Add(fFinalResultsList);
 
 } // void AliAnalysisTaskForStudents::BookAndNestAllLists()
 
 //=======================================================================================================================
 
-void AliAnalysisTaskForStudents::BookControlHistograms()
-{
- // Book all control histograms.
+void AliAnalysisTaskForStudents::BookControlHistograms() {
+  // Book all control histograms.
 
- // a) Book histogram to hold pt spectra;
- // b) ...
+  // a) Book histogram to hold pt spectra:
+  fPtHist = new TH1F("fPtHist", "atrack->Pt()", fNbinsPt, fMinBinPt, fMaxBinPt);
+  fPtHist->SetStats(kFALSE);
+  fPtHist->SetFillColor(kBlue - 10);
+  fPtHist->GetXaxis()->SetTitle("p_{t}");
+  fControlHistogramsList->Add(fPtHist);
 
- // a) Book histogram to hold pt spectra:
- fPtHist = new TH1F("fPtHist","atrack->Pt()",fNbinsPt,fMinBinPt,fMaxBinPt);
- fPtHist->SetStats(kFALSE);
- fPtHist->SetFillColor(kBlue-10);
- fPtHist->GetXaxis()->SetTitle("p_{t}");
- fControlHistogramsList->Add(fPtHist);
- 
- // b) ...
+  // b) Book histogram to hold phi distribution
+  fPhiHist =
+      new TH1F("fPhiHist", "atrack->Phi()", fNbinsPhi, fMinBinPhi, fMaxBinPhi);
+  fPhiHist->SetStats(kFALSE);
+  fPhiHist->SetFillColor(kBlue - 10);
+  fPhiHist->GetXaxis()->SetTitle("#varphi");
+  fControlHistogramsList->Add(fPhiHist);
+
+  // c) Book histogram to hold eta distribution
+  fEtaHist =
+      new TH1F("fEtaHist", "atrack->Eta()", fNbinsEta, fMinBinEta, fMaxBinEta);
+  fEtaHist->SetStats(kFALSE);
+  fEtaHist->SetFillColor(kBlue - 10);
+  fEtaHist->GetXaxis()->SetTitle("#eta");
+  fControlHistogramsList->Add(fEtaHist);
+
+  // d) Book histogram to hold multiplicity distribution
+  fMulHist =
+      new TH1F("fMulHist", "Multiplicity", fNbinsMul, fMinBinMul, fMaxBinMul);
+  fMulHist->SetStats(kFALSE);
+  fMulHist->SetFillColor(kBlue - 10);
+  fMulHist->GetXaxis()->SetTitle("Multiplicity");
+  fControlHistogramsList->Add(fMulHist);
 
 } // void AliAnalysisTaskForStudents::BookControlHistograms()
 
 //=======================================================================================================================
 
-void AliAnalysisTaskForStudents::BookFinalResultsHistograms()
-{
- // Book all histograms to hold the final results.
-
+void AliAnalysisTaskForStudents::BookFinalResultsHistograms() {
+  // Book all histograms to hold the final results.
+  fAveragePhiHist =
+      new TH1F("fAveragePhiHist", "Average #varphi", fNbinsAveragePhi,
+               fMinBinAveragePhi, fMaxBinAveragePhi);
+  fAveragePhiHist->SetStats(kFALSE);
+  fAveragePhiHist->SetFillColor(kBlue - 10);
+  fFinalResultsList->Add(fAveragePhiHist);
 } // void AliAnalysisTaskForStudents::BookFinalResultsHistograms()
 
 //=======================================================================================================================
-
--- PWG/FLOW/Tasks/AliAnalysisTaskForStudents.h
+++ PWG/FLOW/Tasks/AliAnalysisTaskForStudents.h
@@ -1,37 +1,44 @@
-/* 
- * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved. 
- * See cxx source for full Copyright notice 
- * $Id$ 
+/**
+ * File              : AliAnalysisTaskForStudents.h
+ * Author            : Anton Riedel <anton.riedel@tum.de>
+ * Date              : 07.05.2021
+ * Last Modified Date: 08.05.2021
+ * Last Modified By  : Anton Riedel <anton.riedel@tum.de>
+ */
+/*
+ * Copyright(c) 1998-1999, ALICE Experiment at CERN, All rights reserved.
+ * See cxx source for full Copyright notice
+ * $Id$
  */
 
-/************************************** 
-* template class for student projects * 
-**************************************/ 
+/**************************************
+ * template class for student projects *
+ **************************************/
 
 #ifndef ALIANALYSISTASKFORSTUDENTS_H
 #define ALIANALYSISTASKFORSTUDENTS_H
 
-#include "AliAnalysisTaskSE.h"
-#include "AliAODTrack.h"
 #include "AliAODEvent.h"
+#include "AliAODTrack.h"
+#include "AliAnalysisTaskSE.h"
 #include "AliVEvent.h"
 #include "TH1F.h"
 
 //================================================================================================================
 
-class AliAnalysisTaskForStudents : public AliAnalysisTaskSE{
- public:
-  
+class AliAnalysisTaskForStudents : public AliAnalysisTaskSE {
+public:
   AliAnalysisTaskForStudents();
-  AliAnalysisTaskForStudents(const char *name, Bool_t useParticleWeights=kFALSE);
-  virtual ~AliAnalysisTaskForStudents(); 
+  AliAnalysisTaskForStudents(const char *name,
+                             Bool_t useParticleWeights = kFALSE);
+  virtual ~AliAnalysisTaskForStudents();
   virtual void UserCreateOutputObjects();
   virtual void UserExec(Option_t *);
   virtual void Terminate(Option_t *);
-  
+
   // 0.) Methods called in the constructor:
   virtual void InitializeArrays();
- 
+
   // 1.) Methods called in UserCreateOutputObjects():
   virtual void BookAndNestAllLists();
   virtual void BookControlHistograms();
@@ -41,44 +48,82 @@ class AliAnalysisTaskForStudents : public AliAnalysisTaskSE{
   // ...
 
   // 3.) Setters and getters:
-  void SetControlHistogramsList(TList* const chl) {this->fControlHistogramsList = chl;};
-  TList* GetControlHistogramsList() const {return this->fControlHistogramsList;} 
-  void SetFinalResultsList(TList* const frl) {this->fFinalResultsList = frl;};
-  TList* GetFinalResultsList() const {return this->fFinalResultsList;}
+  void SetControlHistogramsList(TList *const chl) {
+    this->fControlHistogramsList = chl;
+  };
+  TList *GetControlHistogramsList() const {
+    return this->fControlHistogramsList;
+  }
+  void SetFinalResultsList(TList *const frl) { this->fFinalResultsList = frl; };
+  TList *GetFinalResultsList() const { return this->fFinalResultsList; }
 
-  void SetPtBinning(Int_t const nbins, Float_t min, Float_t max)
-  {
-   this->fNbinsPt = nbins;
-   this->fMinBinPt = min;
-   this->fMaxBinPt = max;
+  void SetPtBinning(Int_t const nbins, Float_t min, Float_t max) {
+    this->fNbinsPt = nbins;
+    this->fMinBinPt = min;
+    this->fMaxBinPt = max;
+  };
+  void SetPhiBinning(Int_t const nbins, Float_t min, Float_t max) {
+    this->fNbinsPhi = nbins;
+    this->fMinBinPhi = min;
+    this->fMaxBinPhi = max;
+  };
+  void SetEtaBinning(Int_t const nbins, Float_t min, Float_t max) {
+    this->fNbinsEta = nbins;
+    this->fMinBinEta = min;
+    this->fMaxBinEta = max;
   };
 
- private:
-  AliAnalysisTaskForStudents(const AliAnalysisTaskForStudents& aatmpf);
-  AliAnalysisTaskForStudents& operator=(const AliAnalysisTaskForStudents& aatmpf);
-  
-  // 0.) Base lists:
-  TList *fHistList; // base list to hold all output object (a.k.a. grandmother of all lists)
+  void SetMulBinning(Int_t const nbins, Float_t min, Float_t max) {
+    this->fNbinsMul = nbins;
+    this->fMinBinMul = min;
+    this->fMaxBinMul = max;
+  };
 
-  // 1.) Control histograms:  
+  void SetAveragePhiBinning(Int_t const nbins, Float_t min, Float_t max) {
+    this->fNbinsAveragePhi = nbins;
+    this->fMinBinAveragePhi = min;
+    this->fMaxBinAveragePhi = max;
+  };
+
+private:
+  AliAnalysisTaskForStudents(const AliAnalysisTaskForStudents &aatmpf);
+  AliAnalysisTaskForStudents &
+  operator=(const AliAnalysisTaskForStudents &aatmpf);
+
+  // 0.) Base lists:
+  TList *fHistList; // base list to hold all output object (a.k.a. grandmother
+                    // of all lists)
+
+  // 1.) Control histograms:
   TList *fControlHistogramsList; // list to hold all control histograms
   TH1F *fPtHist;                 // atrack->Pt()
   Int_t fNbinsPt;                // number of bins
   Float_t fMinBinPt;             // min bin
   Float_t fMaxBinPt;             // min bin
-  
+  TH1F *fPhiHist;                // atrack->Phi()
+  Int_t fNbinsPhi;               // number of bins
+  Float_t fMinBinPhi;            // min bin
+  Float_t fMaxBinPhi;            // min bin
+  TH1F *fEtaHist;                // atrack->Eta()
+  Int_t fNbinsEta;               // number of bins
+  Float_t fMinBinEta;            // min bin
+  Float_t fMaxBinEta;            // min bin
+  TH1F *fMulHist;                // AliAODEvent->GetMultiplicity()
+  Int_t fNbinsMul;               // number of bins
+  Float_t fMinBinMul;            // min bin
+  Float_t fMaxBinMul;            // min bin
+
   // 2.) Final results:
-  TList *fFinalResultsList; // list to hold all histograms with final results
+  TList *fFinalResultsList;  // list to hold all histograms with final results
+  TH1F *fAveragePhiHist;     // Mean of Phi
+  Int_t fNbinsAveragePhi;    // number of bins
+  Float_t fMinBinAveragePhi; // min bin
+  Float_t fMaxBinAveragePhi; // min bin
 
   // Increase this counter in each new version:
-  ClassDef(AliAnalysisTaskForStudents,1);
-
+  ClassDef(AliAnalysisTaskForStudents, 1);
 };
 
 //================================================================================================================
 
 #endif
-
-
-
-
